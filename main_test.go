e646ac719e3779871cf0645a99c5fbe25aa85f5a
Tony's Macbook <cstony0917@gmail.com>
2015-10-08 17:34:21 +0800

    add test case

--- main_test.go
+++ main_test.go
@@ -24,4 +24,9 @@ func TestTwo(t *testing.T) {
 	if x := Two(in); x != out {
 		t.Errorf("Two(%v) = %v, want %v", in, x, out)
 	}
+
+	const in, out = 333, 2
+	if x := Two(in); x != out {
+		t.Errorf("Two(%v) = %v, want %v", in, x, out)
+	}
 }
