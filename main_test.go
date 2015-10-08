f7cfbd8e8e13ed6a86d36a81eaeb3eccbabf6bb0
Tony's Macbook <cstony0917@gmail.com>
2015-10-08 17:47:01 +0800

    created tdd sample test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    . "github.com/smartystreets/goconvey/convey"
+)
 
 // func TestSqrt(t *testing.T) {
 // 	const in, out = 4, 2
@@ -16,20 +19,18 @@ func TestTwo(t *testing.T) {
 	}
 }
 
+func TestSpec(t *testing.T) {
 
+    // Only pass t into top-level Convey calls
+    Convey("Given some integer with a starting value", t, func() {
+        x := 1
 
+        Convey("When the integer is incremented", func() {
+            x++
 
-	// const in, out = 550000, 2
-	// if x := Two(in); x != out {
-	// 	t.Errorf("Two(%v) = %v, want %v", in, x, out)
-	// }
-
-	// const in, out = 111, 2
-	// if x := Two(in); x != out {
-	// 	t.Errorf("Two(%v) = %v, want %v", in, x, out)
-	// }
-
-	// const in, out = 333, 2
-	// if x := Two(in); x != out {
-	// 	t.Errorf("Two(%v) = %v, want %v", in, x, out)
-	// }
+            Convey("The value should be greater by one", func() {
+                So(x, ShouldEqual, 2)
+            })
+        })
+    })
+}
